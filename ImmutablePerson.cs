b8a39a36dd5e966b45d61722de545b60aaa285fd
Damir Arh <damir.arh@gmail.com>
2020-05-02 12:38:17 +0200

    Implement immutable class and builder

---
+++ ImmutablePerson.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections.Generic;
+
+namespace BuilderImmutableClass
+{
+    public class ImmutablePerson
+    {
+        public string FirstName { get; }
+        public string LastName { get; }
+        public string? MiddleName { get; }
+        public IEnumerable<string> ChildrenNames { get; }
+
+        public ImmutablePerson(string firstName, string lastName, string? middleName = null, IEnumerable<string>? childrenNames = null)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            MiddleName = middleName;
+            ChildrenNames = childrenNames ?? new string[0];
+        }
+
+        private ImmutablePerson(Builder builder)
+        {
+            FirstName = builder.FirstName;
+            LastName = builder.LastName;
+            MiddleName = builder.MiddleName;
+            ChildrenNames = builder.ChildrenNames;
+        }
+
+        public class Builder
+        {
+            internal string FirstName { get; private set; }
+            internal string LastName { get; private set; }
+            internal string? MiddleName { get; private set; }
+            internal IEnumerable<string> ChildrenNames { get; private set; } = new string[0];
+
+            public Builder(string firstName, string lastName)
+            {
+                FirstName = firstName;
+                LastName = lastName;
+            }
+
+            public Builder SetMiddleName(string middleName)
+            {
+                MiddleName = middleName;
+                return this;
+            }
+
+            public Builder SetChildrenNames(IEnumerable<string> childrenNames)
+            {
+                ChildrenNames = childrenNames;
+                return this;
+            }
+
+            public ImmutablePerson Build()
+            {
+                return new ImmutablePerson(this);
+            }
+        }
+    }
+}
---
+++ Tests.cs
@@ -0,0 +1,56 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace BuilderImmutableClass
+{
+    public class Tests
+    {
+        private void AssertImmutablePerson(ImmutablePerson person)
+        {
+            person.FirstName.Should().Be("John");
+            person.LastName.Should().Be("Doe");
+            person.MiddleName.Should().Be("Don");
+            person.ChildrenNames.Should().Equal(new[] { "Jane" });
+        }
+
+        [Test]
+        public void ConstructorCreatesClass()
+        {
+            var person = new ImmutablePerson("John", "Doe", "Don", new[] { "Jane" });
+
+            AssertImmutablePerson(person);
+        }
+
+        [Test]
+        public void ConstructorWithNamedParametersCreatesClass()
+        {
+            var person = new ImmutablePerson(
+                firstName: "John",
+                lastName: "Doe",
+                middleName: "Don",
+                childrenNames: new[] { "Jane" });
+
+            AssertImmutablePerson(person);
+        }
+
+        [Test]
+        public void ConstructorWithoutOptionalParametersCreatesClass()
+        {
+            var person = new ImmutablePerson("John", "Doe");
+
+            person.FirstName.Should().Be("John");
+            person.LastName.Should().Be("Doe");
+        }
+
+        [Test]
+        public void BuilderCreatesClass()
+        {
+            var person = new ImmutablePerson.Builder("John", "Doe")
+                .SetMiddleName("Don")
+                .SetChildrenNames(new[] { "Jane" })
+                .Build();
+
+            AssertImmutablePerson(person);
+        }
+    }
+}
